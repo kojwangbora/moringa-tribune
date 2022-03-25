7a6eb72275c7bde4c0d311e11c4053f26a9efe19
kojwangbora <kojwangbora254@gmail.com>
2022-03-25 12:45:38 +0300

    add search html file

--- news/urls.py
+++ news/urls.py
@@ -5,5 +5,6 @@ urlpatterns=[
     re_path(r'^$',views.news_of_day,name='newsToday'),
     re_path(r'^today/$',views.news_of_day,name='newsToday'),
 
-    re_path(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_news,name = 'pastNews')
+    re_path(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_news,name = 'pastNews'),
+    re_path(r'^search/', views.search_results, name='search_results')
 ]
