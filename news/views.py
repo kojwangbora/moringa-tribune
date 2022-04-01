9e0af95fc6aee04657749919cdcbf03e38a9d9bc
kojwangbora <kojwangbora254@gmail.com>
2022-04-01 15:35:08 +0300

    add email welcom

--- news/views.py
+++ news/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import redirect, render
-from django.http import Http404, HttpResponse
+from django.http import Http404, HttpResponse,HttpResponseRedirect
 import datetime as dt
-from .models import Article
+from .models import Article, NewsLetterRecipients
 from django.core.exceptions import ObjectDoesNotExist
 from .forms import NewsLetterForm
+from .email import send_welcome_email
+ 
 
 # Create your views here.
 def welcome(request):
@@ -25,16 +27,22 @@ def past_days_news(request, past_date):
     return render(request, 'all-news/past-news.html', {"date": date})
 
 def news_of_day(request):
-    date = dt.date.today()
+    date= dt.date.today()
     news = Article.todays_news()
 
     if request.method == 'POST':
-        form = NewsLetterForm(request.POST)
+        form = NewsLetterForm (request.POST)
         if form.is_valid():
-            print('valid')
+            name = form.cleaned_data['your_name']
+            email = form.cleaned_data['email']
+            recepient = NewsLetterRecipients(name=name, email=email)
+            recepient.save()
+            send_welcome_email(name, email)
+            HttpResponseRedirect('news_of_day')
+
     else:
-        form = NewsLetterForm()
-    return render(request, 'all-news/today-news.html', {"date": date,"news":news,"letterForm":form})
+        form = NewsLetterForm
+    return render(request, 'all-news/today-news.html',{"date": date, "news": news,"letterForm": form})
 
 
     return render(request, 'all-news/today-news.html', {"date": date, "news":news})
--- tribune/settings.py
+++ tribune/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 import os
 from pathlib import Path
+# from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -126,6 +127,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
@@ -133,3 +136,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Email configurations remember to install python-decouple
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'kojwangbora254@gmail.com'
+EMAIL_HOST_PASSWORD = 'Access'
